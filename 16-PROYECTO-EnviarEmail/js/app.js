02506469462dc566c49b8f2aceea4cdbc175a30a
Joan Romero <joan.romero@pi.edu.co>
2023-04-18 17:23:12 -0500

    Email Project v1.01

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //Seleccionar los elementos de la interfaz
+  const inputEmail = document.querySelector("#email");
+  const inputAsunto = document.querySelector("#asunto");
+  const inputMensaje = document.querySelector("#mensaje");
+
+  //Asignar eventos
+
+  inputEmail.addEventListener("blur", validar);
+
+  inputAsunto.addEventListener("blur", validar);
+
+  inputMensaje.addEventListener("blur", validar);
+
+  function validar(e) {
+    if (e.target.value.trim() === "") {
+      console.log("Esta vacio");
+    } else {
+      console.log("Si hay texto");
+    }
+  }
+});
