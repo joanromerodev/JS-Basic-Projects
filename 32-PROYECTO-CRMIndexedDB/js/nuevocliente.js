34a4de9812100046e3cba1389dcc4871e4641680
Joan Romero <juanseromerom@hotmail.com>
2023-05-10 23:12:38 -0500

    CRM using IndexedDB v1.1

--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -4,19 +4,24 @@
   const formulario = document.querySelector("#formulario");
 
   document.addEventListener("DOMContentLoaded", () => {
-    conectarDB();
-
     formulario.addEventListener("submit", validarCliente);
+
+    conectarDB();
   });
 
   function conectarDB() {
-    const abrirConexion = window.indexedDB.open("crm", 1);
+    // ABRIR CONEXIÓN EN LA BD:
 
-    abrirConexion.onerror = () => {
+    let abrirConexion = window.indexedDB.open("crm", 1);
+
+    // si hay un error, lanzarlo
+    abrirConexion.onerror = function () {
       console.log("Hubo un error");
     };
 
-    abrirConexion.onsuccess = () => {
+    // si todo esta bien, asignar a database el resultado
+    abrirConexion.onsuccess = function () {
+      // guardamos el resultado
       DB = abrirConexion.result;
     };
   }
@@ -24,19 +29,58 @@
   function validarCliente(e) {
     e.preventDefault();
 
-    //Leer todos los inputs
     const nombre = document.querySelector("#nombre").value;
     const email = document.querySelector("#email").value;
     const telefono = document.querySelector("#telefono").value;
     const empresa = document.querySelector("#empresa").value;
 
     if (nombre === "" || email === "" || telefono === "" || empresa === "") {
-      imprimirAlerta("Todos los campos son obligatorios", "error");
       return;
     }
+
+    // añadir a la BD...
+    // crear un nuevo objeto con toda la info
+
+    const cliente = {
+      nombre,
+      email,
+      telefono,
+      empresa,
+    };
+
+    // Generar un ID único
+    cliente.id = Date.now();
+
+    crearNuevoCliente(cliente);
+  }
+
+  function crearNuevoCliente(cliente) {
+    // NUEVO:
+    const transaction = DB.transaction(["crm"], "readwrite");
+    const objectStore = transaction.objectStore("crm");
+    // console.log(objectStore);
+    objectStore.add(cliente);
+
+    transaction.oncomplete = () => {
+      console.log("Cliente Agregado");
+
+      // Mostrar mensaje de que todo esta bien...
+      imprimirAlerta("Se agregó correctamente");
+
+      setTimeout(() => {
+        window.location.href = "index.html";
+      }, 3000);
+    };
+
+    transaction.onerror = () => {
+      console.log("Hubo un error!");
+      imprimirAlerta("Hubo un Error", "error");
+    };
   }
 
   function imprimirAlerta(mensaje, tipo) {
+    // Crea el div
+
     const divMensaje = document.createElement("div");
     divMensaje.classList.add(
       "px-4",
@@ -45,8 +89,7 @@
       "max-w-lg",
       "mx-auto",
       "mt-6",
-      "text-center",
-      "border"
+      "text-center"
     );
 
     if (tipo === "error") {
@@ -59,8 +102,15 @@
       );
     }
 
+    // Mensaje de error
     divMensaje.textContent = mensaje;
 
+    // Insertar en el DOM
     formulario.appendChild(divMensaje);
+
+    // Quitar el alert despues de 3 segundos
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
   }
 })();
